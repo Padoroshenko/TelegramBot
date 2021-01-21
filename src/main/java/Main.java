c7de8e6be78ed6c000171f9732159cdfa88dace9
NikitaPanin201-341 <bvc341430@gmail.com>
2021-01-21 19:20:44 +0300

    Добавил вывод функций, даты, времени.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,29 @@ import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 public class Main {
     public static void main(String[] args) {
+
+        /*
+        Sedan sedan = new Sedan();//
+        Sedan sedan2 = new Sedan("red",350,"Kia");
+
+        sedan.setColor("green");
+        sedan.setMaxSpeed(300);
+        sedan.setModel("Jeep");
+
+        System.out.println(sedan.getColor());
+        System.out.println(sedan.getMaxSpeed());
+        System.out.println(sedan.getModel());
+
+        sedan.gas();
+
+        System.out.println();
+
+        System.out.println(sedan2.getColor());
+        System.out.println(sedan2.getMaxSpeed());
+        System.out.println(sedan2.getModel());
+
+        sedan2.brake();*/
+
         // Initialize Api Context
         ApiContextInitializer.init();
 
